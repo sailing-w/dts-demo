12b3b057cd113b94b9fe69506c7394ea65254ba2
wujiajie <wujiajie@medlinker.com>
2020-06-12 13:51:55 +0800

    ss

---
+++ go.mod
@@ -0,0 +1,14 @@
+module alibaba-dts-go
+
+go 1.14
+
+require (
+	github.com/LioRoger/subscribe_example v0.0.0-20200310083744-c987318bda6d // indirect
+	github.com/Shopify/sarama v1.26.4
+	github.com/actgardner/gogen-avro/v7 v7.1.0
+	github.com/bsm/sarama-cluster v2.1.15+incompatible
+	github.com/linkedin/goavro v2.1.0+incompatible
+	github.com/onsi/ginkgo v1.12.3 // indirect
+	github.com/sanity-io/litter v1.1.0
+	gopkg.in/linkedin/goavro.v1 v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/sanity-io/litter"
 	"io"
-	"log"
 	"os"
 	"os/signal"
 	"time"
 
 	"github.com/Shopify/sarama"
 	cluster "github.com/bsm/sarama-cluster"
-	"github.com/linkedin/goavro"
-
-	"alibaba-dts-go/avro"
 )
 
 var (
@@ -21,8 +18,8 @@ var (
 
 	// 仅需改动以下配置即可
 	// ***********************************************************
-	kafkaUser       = "username"
-	kafkaPassWord   = "password"
+	kafkaUser       = "data_brigde_sync"
+	kafkaPassWord   = "Medlinker123"
 	kafkaTopic      = "topic"
 	kafkaGroupId    = "groupid"
 	kafkaBrokerList = []string{"localhost:9020"}
@@ -37,11 +34,13 @@ func main() {
 	config.Consumer.Offsets.CommitInterval = 1 * time.Second
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Net.SASL.Enable = true
-	config.Net.SASL.User = kafkaUser + "-" + kafkaGroupId
-	config.Net.SASL.Password = kafkaPassWord
+	config.Net.SASL.User = fmt.Sprintf("%s-%s",
+		"data_brigde_sync", "dtsb2tf52en28mlbel")
+	config.Net.SASL.Password = "Medlinker123"
 	config.Version = sarama.V0_11_0_0
 
-	consumer, err := cluster.NewConsumer(kafkaBrokerList, kafkaGroupId, []string{kafkaTopic}, config)
+	consumer, err := cluster.NewConsumer([]string{"dts-cn-hangzhou.aliyuncs.com:18001"},
+		"dtsb2tf52en28mlbel", []string{"cn_hangzhou_i_bp1ijo6mcpzxkvr3h0in_3306_meddev"}, config)
 	if err != nil {
 		panic(err)
 	}
@@ -60,7 +59,7 @@ func main() {
 	// consume notifications
 	go func() {
 		for ntf := range consumer.Notifications() {
-			fmt.Println("Rebalanced: %+v\n", ntf)
+			fmt.Println("Rebalanced: ", litter.Sdump(ntf))
 		}
 	}()
 
@@ -70,27 +69,32 @@ func main() {
 		case msg, ok := <-consumer.Messages():
 			if ok {
 				r = bytes.NewReader(msg.Value)
+				fmt.Printf("key: %s, value: %s\n", string(msg.Key), string(msg.Value))
 
-				_, err := avro.DeserializeRecord(r)
-				if err != nil {
-					log.Fatal(err)
-				}
+				//record, err := avro.DeserializeRecord(r)
+				//if err != nil {
+				//	log.Fatal("avro.DeserializeRecord is err: ", err)
+				//}
+				//if record.Operation == avro.OperationUPDATE {
+				//	fmt.Println(litter.Sdump(record.Tags))
+				//}
+				//t := avro.NewRecord()
+				//codec, err := goavro.NewCodec(t.Schema())
+				//if err != nil {
+				//	log.Fatal(err)
+				//}
+				//native, _, err := codec.NativeFromBinary(msg.Value)
+				//if err != nil {
+				//	log.Fatal("codec.NativeFromBinary error:", err)
+				//}
+				////fmt.Println("native:", native)
+				//
+				//_, err = codec.TextualFromNative(nil, native)
+				//if err != nil {
+				//	log.Fatal("codec.TextualFromNative: ", err)
+				//}
+				////fmt.Println("texual:", string(texual))
 
-				t := avro.NewRecord()
-				codec, err := goavro.NewCodec(t.Schema())
-				if err != nil {
-					log.Fatal(err)
-				}
-				native, _, err := codec.NativeFromBinary(msg.Value)
-				if err != nil {
-					log.Fatal("error:", err)
-				}
-				fmt.Println("native:", native)
-				texual, err := codec.TextualFromNative(nil, native)
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println("texual:", string(texual))
 			}
 		case <-signals:
 			return
